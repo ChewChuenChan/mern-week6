2248a9b5555c400e2e4ffea05ed756be6c8e25ae
Alice^.^Chan <102845917+ChewChuenChan@users.noreply.github.com>
2022-08-11 14:02:06 -0400

    Delete chew_chan_product_manager_two directory

--- chew_chan_product_manager_two/server/config/mongoose.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require ("mongoose");
-const dbName = "productDB"
-
-mongoose.connect(`mongodb://localhost/${dbName}`)
-    .then(() => console.log(`connected to the db ${dbName}`))
-    .catch((err) => console.log(`Error in db connection to ${dbName}`,err));
-
